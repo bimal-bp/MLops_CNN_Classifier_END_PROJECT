8d82c7c4d409a870658081432062418827ed5aca
bimal-bp <bimalpatrap@gmail.com>
2024-02-08 17:38:57 +0530

    modelApp work complete

--- src/CNNClassifier/pipeline/stage_4_model_evaluation.py
+++ src/CNNClassifier/pipeline/stage_4_model_evaluation.py
@@ -13,7 +13,7 @@ class EvaluationPipeline:
         evaluation = Evaluation(eval_config)
         evaluation.evaluation()
         evaluation.save_score()
-       # evaluation.log_into_mlflow() 
+        evaluation.log_into_mlflow() 
 
 if __name__ == "__main__":
     try:
